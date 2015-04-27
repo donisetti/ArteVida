d5c020d005c10bafcae8365cf2461046455b8ede
Donisetti Ferreira Cosma <doni@vegasoft.com.br>
2015-04-27 18:05:19 -0300

    Arte e Vida : 1.00.6 - 27/04/2015 18:00

--- ArteVida.GestorWeb/ViewModels/AtletaViewModel.cs
+++ ArteVida.GestorWeb/ViewModels/AtletaViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,13 +9,18 @@ namespace ArteVida.GestorWeb.ViewModels
     public class AtletaViewModel
     {
         public int PessoaId { get; set; }
+          [Display(Name = "Ficha nº")]
         public string NumeroFicha { get; set; }
+          [Display(Name = "Nome")]
         public string Nome { get; set; }
+          [Display(Name = "Endereço")]
         public string Endereco { get; set; }
         public string Bairro { get; set; }
         public string Cidade { get; set; }
         public string Estado { get; set; }
+          [Display(Name = "Data Cadastro")]
         public DateTime? DataCadastro { get; set; }
+          [Display(Name = "Data Nascimento")]
         public DateTime? DataNascimento { get; set; }
         public string Rg { get; set; }
         //[StringLength(11)]
@@ -26,8 +32,11 @@ namespace ArteVida.GestorWeb.ViewModels
         public string Escola { get; set; }
         public string Serie { get; set; }
         public string Pai { get; set; }
+          [Display(Name = "Telefone Pai")]
         public string TelefonePai { get; set; }
+          [Display(Name = "Mãe")]
         public string Mae { get; set; }
+          [Display(Name = "Telefone Mãe")]
         public string TelefoneMae { get; set; }
 
         public string Modalidade { get; set; }
