b0afcc40df2adcfde5734afa411e63553c935138
Donisetti <donisetti@msn.com>
2015-06-16 12:59:16 -0300

    Arte e Vida : 1.00.7 - 16/06/2015 13:00

--- ArteVida.GestorWeb/Controllers/HomeController.cs
+++ ArteVida.GestorWeb/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace ArteVida.GestorWeb.Controllers
 
         public ActionResult Index()
         {
-            ViewBag.Versao = "Arte e Vida : 1.00.7 - 12/06/2015 16:30 ";
+            ViewBag.Versao = "Arte e Vida : 1.00.7 - 16/06/2015 13:00 ";
 
             //ViewBag.Extenso = Extenso.ToExtenso(245765.32, Extenso.TipoValorExtenso.Monetario);
 
