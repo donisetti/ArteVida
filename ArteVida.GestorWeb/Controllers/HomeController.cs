25fa0aa022d715a83579249efdd22e5ef3ccd355
Donisetti Ferreira Cosma <doni@vegasoft.com.br>
2015-11-01 18:00:15 -0300

    Arte e Vida : 1.00.13 - 01/11/2015 18:00 report

--- ArteVida.GestorWeb/Controllers/HomeController.cs
+++ ArteVida.GestorWeb/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace ArteVida.GestorWeb.Controllers
 
         public ActionResult Index()
         {
-            ViewBag.Versao = "Arte e Vida : 1.00.13 - 01/11/2015 18:00 ";
+            ViewBag.Versao = "Arte e Vida : 1.00.13 - 01/11/2015 18:00 report";
             return View();
         }
 
