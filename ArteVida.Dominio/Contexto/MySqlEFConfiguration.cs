e673fda6cfe3a037fe992f3856f5d8d83fd6b53b
Donisetti Ferreira Cosma <doni@vegasoft.com.br>
2015-12-04 16:11:24 -0200

    Arte e Vida : 1.00.14 - 04/12/2015 16:10

---
+++ ArteVida.Dominio/Contexto/MySqlEFConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.Entity;
+
+namespace ArteVida.Dominio.Contexto
+{
+    public class MySqlEFConfiguration : DbConfiguration
+    {
+        public MySqlEFConfiguration()
+        {
+            SetHistoryContext("MySql.Data.MySqlClient", (conn, schema) => new MySqlHistoryContext(conn, schema));
+        }
+    }
+}
